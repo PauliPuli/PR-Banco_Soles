608ba4c578395021cc46dd0522ab272f3c3fa7ff
Paulina San Martin <puli.sanmartin@gmail.com>
2024-04-29 19:44:59 -0400

    Cambio estilo html y adición carpeta assets

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import router from "./routes/routes.js";
 //Midleware
 app.use(express.json());
 app.use(express.static("img"));
+app.use(express.static('assets'));
 app.use("/",router);
 
 app.listen(PORT, () =>
